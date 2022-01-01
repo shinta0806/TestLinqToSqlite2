8ebdbb08e202bfd80ce5e4264b6258dd51dd3b41
shinta0806 <shinta.0806@gmail.com>
2022-01-01 22:04:22 +0900

    ジャーナルモード切替可能にした

--- BasicDatabase/TestContext.cs
+++ BasicDatabase/TestContext.cs
@@ -50,6 +50,15 @@ namespace TestLinqToSqlite2.BasicDatabase
 				DataSource = "Test.sqlite3",
 			};
 			using SqliteConnection sqliteConnection = new(stringBuilder.ToString());
+
+#if false
+			// EF Core ではデフォルトでジャーナルモードが WAL になっています。DELETE にしたい場合は以下を実行します。
+			sqliteConnection.Open();
+			using SqliteCommand command = sqliteConnection.CreateCommand();
+			command.CommandText = @"PRAGMA journal_mode = 'delete'";
+			command.ExecuteNonQuery();
+#endif
+
 			optionsBuilder.UseSqlite(sqliteConnection);
 		}
 	}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ﻿# C# で SQLite を便利に使うサンプルコード【改訂版】
+
+![実行イメージ](Run.png)
+
+詳細：https://shinta0806be.ldblog.jp/archives/10472748.html
